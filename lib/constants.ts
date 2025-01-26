a3ecc3554004fdbceb82abe0d821033f5750e71d
the-dev-syntax <the.dev.syntax@gmail.com>
2025-01-26 22:33:13 +0300

    add # marker to env alternative

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,6 @@
-export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Essential-Vital'
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || '#Essential-Vital'
 export const APP_SLOGAN =
-  process.env.NEXT_PUBLIC_APP_SLOGAN || 'Be Healthy, Be Happy.'
+  process.env.NEXT_PUBLIC_APP_SLOGAN || '#Be Healthy, Be Happy.'
 export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
-  'Discover premium, science-backed supplements designed to support your health and wellness goals. Shop natural, high-quality vitamins, minerals, and herbal formulas for energy, immunity, and overall well-being.'
+  '#Discover premium, science-backed supplements designed to support your health and wellness goals. Shop natural, high-quality vitamins, minerals, and herbal formulas for energy, immunity, and overall well-being.'
