c87f20d6aa64b81fde2b24ffe718396d4c750714
the-dev-syntax <the.dev.syntax@gmail.com>
2025-01-27 18:57:37 +0300

    carosel

--- lib/data.ts
+++ lib/data.ts
@@ -33,6 +33,29 @@ const data = {
       href: '/page/help',
     },
   ],
+  carousels: [
+    {
+      title: 'Most Popular supplement',
+      buttonCaption: 'Shop Now',
+      image: '/images/banner3.jpg',
+      url: '/search?category=Shoes',
+      isPublished: true,
+    },
+    {
+      title: 'Best Sellers in supplement',
+      buttonCaption: 'Shop Now',
+      image: '/images/banner1.jpg',
+      url: '/search?category=T-Shirts',
+      isPublished: true,
+    },
+    {
+      title: 'Best Deals on supplement',
+      buttonCaption: 'See More',
+      image: '/images/banner2.jpg',
+      url: '/search?category=Wrist Watches',
+      isPublished: true,
+    },
+  ],
 }
 
 export default data
